722343320cb402e489cd853088091eb0535fef7d
SINGH Domeshwar INNOV/IT-S <domeshwar.singh@orange.com>
2021-07-27 06:42:19 +0530

    First groovy

---
+++ src/com/testing/test/india.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/groovy
+def call(Map config) {
+		if (country == "india") {
+				return "Delhi is the Captital of india"
+			}
+			else
+			{
+				return "Delhi is not the capital of India"
+			} 
+}
